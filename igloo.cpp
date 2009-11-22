27fbb8ed48f3e484a356bf4b5610420b14eb51f1
Joakim Karlsson <joakim@jokl.local>
2009-11-22 13:55:51 +0100

    Test med functors

--- igloo.cpp
+++ igloo.cpp
@@ -1,5 +1,5 @@
 #include <map>    
-
+#include <vector>
 using namespace std;
  
 
@@ -10,20 +10,31 @@ class TestFixtureFactory
 class TestFixtureBase
 {     
 public:
-	virtual void run() = 0;
+	virtual void Run() = 0;
+	virtual void RegisterTestMethods() {}      
+};
+   
+class TestFunctor
+{
+public:
+	virtual void call() = 0;     
 };
 
-template <typename T>
+template <class T>
 class TestFixture : public TestFixtureBase
-{  
-	typedef void (*TestMethod)();	
-	map<string, TestMethod> _testMethods;
+{
+public:     
+ 	vector<TestFunctor*> _testMethods;  
 	
-public:
-	void run()
+   void Run() 
+   {  
+	T t;                
+	vector<TestFunctor*> testMethods = t.GetTestMethods();
+	for(vector<TestFunctor*>::iterator it = testMethods.begin(); it != testMethods.end(); it++)
 	{
-		
-	}	 
+		(*it)->call();
+	}
+   }     
 };
 
 typedef map<string, TestFixtureBase* > TestFixtureMap;
@@ -32,28 +43,34 @@ TestFixtureMap fixtureMap;
 int RegisterTestFixture(string name, TestFixtureBase* testFixture)
 {        
 	fixtureMap[name] = testFixture; 
+	testFixture->RegisterTestMethods();
 	return 0;
 }  
 
 #define TestFixture(fixture) \
 class fixture; \
 int fixture##_dummy = RegisterTestFixture( #fixture , new TestFixture<fixture>()); \
-class fixture
-
-
+class fixture : public TestFixture<fixture>
+                                       
 TestFixture(MyTestCase)
-{   
+{      			
+public:      
+	
+	class Test1 : public TestFunctor
+	{
+	  void call()
+	  {
+		cout << "Running Test1" << endl;   	
+	  }
+    };
+	                                
+	vector<TestFunctor*> GetTestMethods()
+	{    
+		vector<TestFunctor*> testMethods;
+		testMethods.push_back(new Test1());
+		return testMethods;
+	}
 };
-
-TestFixture(MySecondTestCase)
-{
-public:
-   	void Test()
-    {
-		cout << "Running test" << endl;
-    }
-};
-                            
 
 int main()
 {            
@@ -62,7 +79,7 @@ int main()
 	for(TestFixtureMap::iterator it = fixtureMap.begin(); it != fixtureMap.end(); it++)
 	{
 		cout << "Running test: " << (*it).first << endl;
-		(*it).second->run();
+		(*it).second->Run();
 	}
 	
 	return 0;
