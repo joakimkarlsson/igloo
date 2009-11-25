d5eb80b4e9037ea7aa17e1b0184944ea38d1d423
Joakim Karlsson <joakim@jokl.local>
2009-11-25 21:47:56 +0100

    Using boost preprocessor helpers for repetition of GetTextXX methods

--- igloo.cpp
+++ igloo.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>    
-#include <vector>
+#include <vector>    
+#include <boost/preprocessor.hpp>
 using namespace std;
                                            
 class TestFixtureBase
@@ -8,6 +9,10 @@ class TestFixtureBase
 public:
 	virtual void Run() = 0;    
 };
+          
+#ifndef MAX_NUMBER_OF_TEST_METHODS   
+  #define MAX_NUMBER_OF_TEST_METHODS 20
+#endif
 
 template <typename T>
 class TestFixture : public TestFixtureBase
@@ -30,54 +35,18 @@ public:
 	   (t.*method)();
 	}
    }           
-   
+        
 	void GetTests(TestMethods& testMethods)
-	{
-		GetTest0(testMethods);
-		GetTest1(testMethods);
-		GetTest2(testMethods);
-		GetTest3(testMethods);
-		GetTest4(testMethods);
-		GetTest5(testMethods);
-		GetTest6(testMethods);
-		GetTest7(testMethods);
-		GetTest8(testMethods);
-		GetTest9(testMethods);
-		GetTest10(testMethods);
-		GetTest11(testMethods);
-		GetTest12(testMethods);
-		GetTest13(testMethods);
-		GetTest14(testMethods);
-		GetTest15(testMethods);
-		GetTest16(testMethods);
-		GetTest17(testMethods);
-		GetTest18(testMethods);
-		GetTest19(testMethods);
-		GetTest20(testMethods);
+	{                                                
+		#define REPEAT_SIGNATURE(z,n,name) name ## n (testMethods);
+		BOOST_PP_REPEAT(MAX_NUMBER_OF_TEST_METHODS, REPEAT_SIGNATURE, GetTest)
+		#undef REPEAT_SIGNATURE 
 	}
-
-   virtual void GetTest0(TestMethods&) { } 
-   virtual void GetTest1(TestMethods&) { } 
-   virtual void GetTest2(TestMethods&) { } 
-   virtual void GetTest3(TestMethods&) { } 
-   virtual void GetTest4(TestMethods&) { } 
-   virtual void GetTest5(TestMethods&) { } 
-   virtual void GetTest6(TestMethods&) { } 
-   virtual void GetTest7(TestMethods&) { }
-   virtual void GetTest8(TestMethods&) { }
-   virtual void GetTest9(TestMethods&) { }
-   virtual void GetTest10(TestMethods&) { }
-   virtual void GetTest11(TestMethods&) { }
-   virtual void GetTest12(TestMethods&) { }
-   virtual void GetTest13(TestMethods&) { }
-   virtual void GetTest14(TestMethods&) { }
-   virtual void GetTest15(TestMethods&) { }
-   virtual void GetTest16(TestMethods&) { }
-   virtual void GetTest17(TestMethods&) { }
-   virtual void GetTest18(TestMethods&) { }
-   virtual void GetTest19(TestMethods&) { }
-   virtual void GetTest20(TestMethods&) { }
- 
+     
+	#define REPEAT_SIGNATURE(z,n,name) virtual void name ## n (TestMethods&) {}
+	BOOST_PP_REPEAT(MAX_NUMBER_OF_TEST_METHODS, REPEAT_SIGNATURE, GetTest)
+	#undef REPEAT_SIGNATURE
+                    
 };
 
 class TestRunner
@@ -108,13 +77,8 @@ class fixture; \
 int fixture##_dummy = RegisterTestFixture( #fixture , new TestFixture<fixture>()); \
 class fixture : public TestFixture<fixture>            
                      
-#define APPENDCOUNTER_IMPL(x, l) x##l
-#define APPENDCOUNTER_DETAIL(x, l) APPENDCOUNTER_IMPL(x, l)
-#define APPENDCOUNTER(x) APPENDCOUNTER_DETAIL(x, __COUNTER__)
-
-
 #define TestMethod(fixture, method) \
-void APPENDCOUNTER(GetTest)(vector< void (fixture::*)() >& tests) \
+void BOOST_PP_CAT(GetTest, __COUNTER__)(vector< void (fixture::*)() >& tests) \
 { \
 	tests.push_back(&fixture::method); \
 } \
@@ -126,12 +90,12 @@ public:
 
 	TestMethod(MyTestCase, Test1)
     {
-		cout << "  Running Test1" << endl;   	
+		cout << "  Running Test1!" << endl;   	
     }
 
  	TestMethod(MyTestCase, Test2)
 	{
-		cout << "  Running Test2" << endl;   	
+		cout << "  Running Test2!" << endl;   	
 	}
 };
 
