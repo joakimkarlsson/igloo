2f4df662356f78bf037ce1753c40ad898561e463
Joakim Karlsson <joakim@jokl.local>
2009-12-09 12:34:22 +0100

    Integer operators only

--- igloo.cpp
+++ igloo.cpp
@@ -2,13 +2,50 @@
 #include "igloo.hpp"
 #include "constraints/is.h"
 
-TestFixture(AssertionTests)
+TestFixture(Assertions)
 {
   public:
 
-  TestMethod(AssertionTests, Experimental)
+  TestMethod(Assertions, ShouldHandleIntegerEquality)
   {
-    Assert::That(5, Is().Not().Not().EqualTo(5));
+    Assert::That(5, Is().EqualTo(5));
+  }
+
+  TestMethod(Assertions, ShouldDetectIntegerInequality)
+  {
+    bool exceptionIsThrown = false;
+
+    try
+    {
+      Assert::That(5, Is().EqualTo(4));
+    }
+    catch (AssertionException)
+    {
+      exceptionIsThrown = true;
+    }
+
+    Assert::That(exceptionIsThrown);
+  }
+
+  TestMethod(Assertions, ShouldHandleNotOperators)
+  {
+    Assert::That(5, Is().Not().EqualTo(4));
+  }
+
+  TestMethod(Assertions, ShouldDetectIfNotFails)
+  {
+    bool exceptionIsThrown = false;
+
+    try
+    {
+      Assert::That(5, Is().Not().EqualTo(5));
+    }
+    catch (AssertionException)
+    {
+      exceptionIsThrown = true;
+    }
+
+    Assert::That(exceptionIsThrown);
   }
 };
 
