b9142ee26bdf24f19f6cc2c80068182a52c6db3b
Joakim Karlsson <joakim@jokl.local>
2009-12-13 10:57:05 +0100

    Refactoring

--- constraints/assertions.h
+++ constraints/assertions.h
@@ -5,27 +5,19 @@
 class Assert
 {
 public:
+
   template <typename T>
   static void That(T actual, ConstraintExpression& constraint)
   {
     stack<bool> resultStack;
 
     IOperator* op;
-    while((op = constraint.PopOperator()) != NULL)
+    while ((op = constraint.PopOperator()) != NULL)
     {
-      if(op->IsLogicalOperator())
-      {
-        LogicalOperator* logical = dynamic_cast<LogicalOperator*>(op);
-        logical->Evaluate(resultStack);
-      }
-      else
-      {
-        Constraint<T>* constraint = dynamic_cast<Constraint<T>* >(op);
-        resultStack.push(constraint->Evaluate(actual));
-      }
+      PerformOperation(op, actual, resultStack);
     }
 
-    if(!resultStack.top())
+    if (!resultStack.top())
     {
       throw AssertionException("WTF!?");
     }
@@ -33,9 +25,25 @@ public:
 
   static void That(bool& actual)
   {
-    if(!actual)
-      {
+    if (!actual)
+    {
       throw AssertionException("Expected condition to be true");
+    }
+  }
+
+private:
+  template <typename T>
+  static void PerformOperation(IOperator * op, T actual, stack<bool>& resultStack)
+  {
+    if (op->IsLogicalOperator())
+    {
+      LogicalOperator* logical = dynamic_cast<LogicalOperator*> (op);
+      logical->Evaluate(resultStack);
+    }
+    else
+    {
+      Constraint<T>* constraint = dynamic_cast<Constraint<T>*> (op);
+      resultStack.push(constraint->Evaluate(actual));
     }
   }
 };
