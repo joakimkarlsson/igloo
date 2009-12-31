e2c26b991e914104ed21eb2bf0ad4603a8e8ef08
Joakim Karlsson <joakim@jkarlsson.com>
2009-12-31 12:05:09 +0100

    Cleaned up syntax header file

--- syntax.h
+++ syntax.h
@@ -1,13 +1,11 @@
 #ifndef _SYNTAX_H_
 #define _SYNTAX_H_
 
-#include <memory>
-
 #include "expressions.h"
 
-#include "syntax/iexpressionowner.h"
 #include "syntax/constraintoperations.h"
 #include "syntax/stringconstraintoperations.h"
+
 #include "syntax/rootnode.h"
 
 #endif
