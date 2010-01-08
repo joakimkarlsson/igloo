48ecdae7ee9961a144bc069221cc5207ae167dc0
Joakim Karlsson <joakim@jkarlsson.com>
2010-01-08 15:37:37 +0100

    Removed unnecessary template parameters

--- syntax/constraintoperations.h
+++ syntax/constraintoperations.h
@@ -27,69 +27,59 @@ class ConstraintNode;
       ConstraintNode<ExpressionItem<EqualToConstraint<ExpectedType>, ExpressionItemType> > EqualTo(ExpectedType expected)
     {
       typedef EqualToConstraint<ExpectedType> ConstraintType;
-
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
     ConstraintNode<ExpressionItem<EqualToConstraint<std::string>, ExpressionItemType> > EqualTo(const char* expected)
     {
       typedef EqualToConstraint<std::string> ConstraintType;
-
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
     template <typename ExpectedType>
     ConstraintNode<ExpressionItem<GreaterThanConstraint<ExpectedType>, ExpressionItemType> > GreaterThan(ExpectedType expected)
     {
       typedef GreaterThanConstraint<ExpectedType> ConstraintType;
-
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
     template <typename ExpectedType>
       ConstraintNode<ExpressionItem<LessThanConstraint<ExpectedType>, ExpressionItemType> > LessThan(ExpectedType expected)
     {
       typedef LessThanConstraint<ExpectedType> ConstraintType;
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
     template <typename ExpectedType>
     ConstraintNode<ExpressionItem<ContainingConstraint<ExpectedType>, ExpressionItemType> > Containing(ExpectedType expected)
     {
       typedef ContainingConstraint<ExpectedType> ConstraintType;
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
     ConstraintNode<ExpressionItem<StartingWithConstraint, ExpressionItemType> > StartingWith(std::string expected)
     {
       typedef StartingWithConstraint ConstraintType;
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
     ConstraintNode<ExpressionItem<EndingWithConstraint, ExpressionItemType> > EndingWith(std::string expected)
     {
       typedef EndingWithConstraint ConstraintType;
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
    ConstraintNode<ExpressionItem<OfLengthConstraint, ExpressionItemType> > OfLength(int expected)
     {
       typedef OfLengthConstraint ConstraintType;
-      typedef ExpressionItem<ConstraintType, ExpressionItemType> NextExpressionItemType;    
-      return CreateNode<ConstraintType, NextExpressionItemType>(new ConstraintType(expected));
+      return CreateNode(new ConstraintType(expected));
     }
 
   private:
-    template <typename ConstraintType, typename NewExpressionItemType>
-      ConstraintNode<NewExpressionItemType> CreateNode(ConstraintType* constraint)
+    template <typename ConstraintType>
+      ConstraintNode<ExpressionItem<ConstraintType, ExpressionItemType> > CreateNode(ConstraintType* constraint)
     {
+      typedef ExpressionItem<ConstraintType, ExpressionItemType> NewExpressionItemType;
       boost::shared_ptr<NewExpressionItemType> expressionItem(new NewExpressionItemType(boost::shared_ptr<ConstraintType>(constraint), Base::m_expressionItem));
       ConstraintNode<NewExpressionItemType> node(expressionItem);
       return node;
