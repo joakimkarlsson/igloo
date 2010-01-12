6c9bd7ec5c469a6180cf7e13ecabade56db58a4c
Joakim Karlsson <joakim@jkarlsson.com>
2010-01-12 16:22:56 +0100

    Removed auto_ptr from container tests

--- tests/container_tests.cpp
+++ tests/container_tests.cpp
@@ -9,33 +9,27 @@ TestFixture(ContainerConstraints)
 
   void SetUp()
   {
-    m_vector = std::auto_ptr<IntVector>(new IntVector());
-    m_vector->push_back(123);
-    m_vector->push_back(43);
-    m_vector->push_back(25);
-    m_vector->push_back(54);
+    m_vector.clear();
+    m_vector.push_back(123);
+    m_vector.push_back(43);
+    m_vector.push_back(25);
+    m_vector.push_back(54);
   }
 
   TestMethod(ShouldHandleContainingForVectors)
   {
-    const IntVector& v = *(m_vector.get());
-
-    Assert::That(v, Is().Containing(43));
+    Assert::That(m_vector, Is().Containing(43));
   }
 
   TestMethod(ShouldHandleOfLengthForVectors)
   {
-    const IntVector& v = *(m_vector.get());
-
-    Assert::That(v, Is().OfLength(4));
+    Assert::That(m_vector, Is().OfLength(4));
   }
 
   TestMethod(ShouldHandleFailingOfLengthForVectors)
   {
-    const IntVector& v = *(m_vector.get());
-
-    AssertTestFails(Assert::That(v, Is().OfLength(7)), "of length 7\nActual:");
+    AssertTestFails(Assert::That(m_vector, Is().OfLength(7)), "of length 7\nActual:");
   }
 
-  std::auto_ptr<IntVector> m_vector;
+  IntVector m_vector;
 };
