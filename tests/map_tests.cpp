f6aa82f18681ce3d652c3e6bf3090e83fac9f63d
Joakim Karlsson <joakim@jkarlsson.com>
2010-01-20 09:36:35 +0100

    Added test for expression templates

--- tests/map_tests.cpp
+++ tests/map_tests.cpp
@@ -24,3 +24,26 @@ TestFixture(MapTests)
     AssertTestFails(Assert::That(ages, Is().Not().Containing("hanna")), "Expected: not contains hanna"); 
   }
 };
+
+TestFixture(MapTests_ExpressionTemplates)
+{
+  std::map<std::string, int> ages;
+  
+  void SetUp()
+  {
+    ages["joakim"] = 38;
+    ages["maria"] = 36;
+    ages["hanna"] = 6;
+    ages["moa"] = 4;
+  }
+  
+  TestMethod(ContainingShouldDetermineIfKeyExists)
+  {
+    Assert::That(ages, Contains("joakim"));
+  }
+  
+  TestMethod(ShouldGiveAProperMessageWhenContainingFails)
+  {
+    AssertTestFails(Assert::That(ages, !Contains("hanna")), "Expected: not contains hanna"); 
+  }
+};
