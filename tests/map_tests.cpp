8ae51cb3472df603da647f8e1b22d5292a8d92d5
Joakim Karlsson <joakim@jkarlsson.com>
2010-01-20 09:15:35 +0100

    Recreated missing test

--- tests/map_tests.cpp
+++ tests/map_tests.cpp
@@ -1,11 +1,21 @@
-/*
- *  map_tests.cpp
- *  igloo_xcode
- *
- *  Created by Joakim Karlsson on 2010-01-20.
- *  Copyright 2010 Blueplane. All rights reserved.
- *
- */
+#include "igloo_self_test.h"
 
-#include "map_tests.h"
+using namespace igloo;
 
+TestFixture(MapTests)
+{
+  std::map<std::string, int> ages;
+  
+  void SetUp()
+  {
+    ages["joakim"] = 38;
+    ages["maria"] = 36;
+    ages["hanna"] = 6;
+    ages["moa"] = 4;
+  }
+  
+  TestMethod(ContainingShouldDetermineIfKeyExists)
+  {
+    Assert::That(ages, Is().Containing("joakim"));
+  }
+};
