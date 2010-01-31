671a4329451154d7c7799c9823a6e1ccc5f4c91e
Kim Grasman <kim.grasman@gmail.com>
2010-01-31 10:33:26 +0100

    Cleaned up stringize_tests

--- tests/stringize_tests.cpp
+++ tests/stringize_tests.cpp
@@ -1,83 +1,74 @@
 #include "igloo_self_test.h"
 using namespace igloo;
 
-
-struct HasStreamOp
+namespace
 {
-  HasStreamOp(int id)
-    : m_id(id)
+  // No overload for operator<<(std::ostream&) or igloo::Stringize
+  struct WithoutStreamOperator
   {
-  }
- 
-  int m_id;
-};
- 
-std::ostream& operator<<(std::ostream& stream, const HasStreamOp& a)
-{
-  stream << a.m_id;
-  return stream;
-}
-
-bool operator==(const HasStreamOp& lhs, const HasStreamOp& rhs)
-{
-  return lhs.m_id == rhs.m_id;
-}
- 
-struct NoStreamOp
-{
-  NoStreamOp(int id)
+    WithoutStreamOperator(int id)
     : m_id(id)
+    {
+    }
+
+    bool operator==(const WithoutStreamOperator& rhs) const
+    {
+    return m_id == rhs.m_id;
+    }
+
+    int m_id;
+  };
+
+  // Has operator<<(std::ostream&)
+  struct WithStreamOperator : public WithoutStreamOperator
   {
-  }
- 
-  int m_id;
-};
-
-bool operator==(const NoStreamOp& lhs, const NoStreamOp& rhs)
-{
-  return lhs.m_id == rhs.m_id;
-}
-
-struct NoStreamOpButWithTraits
-{
-  NoStreamOpButWithTraits(int id)
-    : m_id(id)
+    WithStreamOperator(int id)
+    : WithoutStreamOperator(id)
+    {
+    }
+  };
+   
+  std::ostream& operator<<(std::ostream& stream, const WithStreamOperator& a)
   {
+    stream << a.m_id;
+    return stream;
   }
- 
-  int m_id;
-};
 
-bool operator==(const NoStreamOpButWithTraits& lhs, const NoStreamOpButWithTraits& rhs)
-{
-  return lhs.m_id == rhs.m_id;
+  // Has no operator<<(std::ostream&), but an overload for igloo::Stringize
+  struct WithoutStreamOperatorButWithStringizeOverload : public WithoutStreamOperator
+  {
+    WithoutStreamOperatorButWithStringizeOverload(int id)
+    : WithoutStreamOperator(id)
+    {
+    }
+  };
+
+  inline std::string Stringize(const WithoutStreamOperatorButWithStringizeOverload& t)
+  {
+    return igloo::Stringize(t.m_id);
+  }
 }
-
-inline std::string Stringize(const NoStreamOpButWithTraits& t)
-{
-  return Stringize(t.m_id);
-}  
 
 TestFixture(StringizeTests)
 {
   TestMethod(ShouldHandleTypesWithStreamOperators)
   {
-    HasStreamOp a(12);
-    HasStreamOp b(13);
+    WithStreamOperator a(12);
+    WithStreamOperator b(13);
     AssertTestFails(Assert::That(a, Is().EqualTo(b)), "Expected: equal to 13\nActual: 12");
   }
 
   TestMethod(ShouldHandleTypesWithoutStreamOperators)
   {
-    NoStreamOp a(12);
-    NoStreamOp b(13);
+    WithoutStreamOperator a(12);
+    WithoutStreamOperator b(13);
     AssertTestFails(Assert::That(a, Is().EqualTo(b)), "Expected: equal to [unsupported type]\nActual: [unsupported type]");
   }
 
   TestMethod(ShouldHandleTypesWithTraits)
   {
-    NoStreamOpButWithTraits a(12);
-    NoStreamOpButWithTraits b(13);
+    WithoutStreamOperatorButWithStringizeOverload a(12);
+    WithoutStreamOperatorButWithStringizeOverload b(13);
     AssertTestFails(Assert::That(a, Is().EqualTo(b)), "Expected: equal to 13\nActual: 12");
   }
 }; 
@@ -86,22 +77,22 @@ TestFixture(StringizeTestsExpressionTemplates)
 {
   TestMethod(ShouldHandleTypesWithStreamOperators)
   {
-    HasStreamOp a(12);
-    HasStreamOp b(13);
+    WithStreamOperator a(12);
+    WithStreamOperator b(13);
     AssertTestFails(Assert::That(a, Equals(b)), "Expected: equal to 13\nActual: 12");
   }
 
   TestMethod(ShouldHandleTypesWithoutStreamOperators)
   {
-    NoStreamOp a(12);
-    NoStreamOp b(13);
+    WithoutStreamOperator a(12);
+    WithoutStreamOperator b(13);
     AssertTestFails(Assert::That(a, Equals(b)), "Expected: equal to [unsupported type]\nActual: [unsupported type]");
   }
 
   TestMethod(ShouldHandleTypesWithTraits)
   {
-    NoStreamOpButWithTraits a(12);
-    NoStreamOpButWithTraits b(13);
+    WithoutStreamOperatorButWithStringizeOverload a(12);
+    WithoutStreamOperatorButWithStringizeOverload b(13);
     AssertTestFails(Assert::That(a, Is().EqualTo(b)), "Expected: equal to 13\nActual: 12");
   }
 }; 
