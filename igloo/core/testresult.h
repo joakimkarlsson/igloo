e93569905d6a7dd78f9adb22eb18d17b3d04e7ed
Joakim Karlsson <joakim@jokl.local>
2010-02-16 08:05:05 +0100

    Added license notice

--- igloo/core/testresult.h
+++ igloo/core/testresult.h
@@ -1,9 +1,8 @@
-/* 
-* File:   testresult.h
-* Author: joakim
-*
-* Created on December 8, 2009, 8:32 PM
-*/
+
+//          Copyright Joakim Karlsson & Kim Gräsman 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef IGLOO_TESTRESULT_H
 #define	IGLOO_TESTRESULT_H
