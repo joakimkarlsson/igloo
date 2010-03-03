aa97923ecd49d6e68e850b8a3b7dbc0e7038771d
Joakim Karlsson <joakim@jkarlsson.com>
2010-03-03 20:33:22 +0100

    Reintroduced TestFixture/TestMethod for backward compatibility

--- igloo/core/registration.h
+++ igloo/core/registration.h
@@ -39,4 +39,37 @@ struct SpecRegistrar_##spec \
 } SpecRegistrar_##spec; \
 virtual void spec()
 
+
+#define TestFixture(context) \
+struct context; \
+struct ContextRegistrar_##context \
+{ \
+ContextRegistrar_##context() \
+{ \
+igloo::TestRunner::RegisterContext<igloo::ContextRunner<void, context> >(#context); \
+} \
+} context##_IglooRegistrar; \
+struct context : public ContextProvider<context, IGLOO_CURRENT_CONTEXT>
+
+#define DerivedFixture(context, baseContext) \
+struct context; \
+struct ContextRegistrar_##context \
+{ \
+ContextRegistrar_##context() \
+{ \
+igloo::TestRunner::RegisterContext<igloo::ContextRunner<baseContext, context> >(#context); \
+} \
+} context##_IglooRegistrar; \
+struct context : public baseContext
+
+#define TestMethod(spec) \
+struct SpecRegistrar_##spec \
+{ \
+SpecRegistrar_##spec() \
+{ \
+ContextRegistry<IGLOO_CURRENT_CONTEXT>::RegisterSpec(#spec, &IGLOO_CURRENT_CONTEXT::spec); \
+} \
+} SpecRegistrar_##spec; \
+virtual void spec()
+
 #endif
