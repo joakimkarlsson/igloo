ebe901d8610810b41b14650567424aeb5225ee30
Kim Grasman <kim.grasman@gmail.com>
2010-03-27 08:48:02 +0100

    Changed friend definition to declaration + separate, free function. Formatting.

--- igloo/core/testresult.h
+++ igloo/core/testresult.h
@@ -1,17 +1,17 @@
-
 //          Copyright Joakim Karlsson & Kim Gräsman 2010.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef IGLOO_TESTRESULT_H
-#define	IGLOO_TESTRESULT_H
+#define IGLOO_TESTRESULT_H
 
 namespace igloo {
+
   class TestResult
   {
   public:
-     TestResult(const std::string& contextName, const std::string& specName, bool success, const std::string& errorMessage)
+    TestResult(const std::string& contextName, const std::string& specName, bool success, const std::string& errorMessage)
       : m_contextName(contextName), m_specName(specName), m_success(success), m_errorMessage(errorMessage)
     {
     }
@@ -41,15 +41,10 @@ namespace igloo {
       return m_contextName == rhs.m_contextName &&
       m_specName == rhs.m_specName &&
       m_success == rhs.m_success &&
-      m_errorMessage == rhs.m_errorMessage;    
+      m_errorMessage == rhs.m_errorMessage;
     }
     
-    friend std::ostream& operator<<(std::ostream& stream, const TestResult& res)
-    {
-      stream << "TestResult ( m_contextName=" << res.m_contextName << ", m_specName=" << res.m_specName << ", m_success=" << res.m_success
-      << ", m_errorMessage=" << res.m_errorMessage << " )";
-      return stream;
-    } 
+    friend std::ostream& operator<<(std::ostream& stream, const TestResult& res);
 
   private:
     std::string m_contextName;
@@ -57,8 +52,19 @@ namespace igloo {
     bool m_success;
     std::string m_errorMessage;
   };
-  
- 
+
+  inline std::ostream& operator<<(std::ostream& stream, const TestResult& res)
+  {
+    stream
+      << "TestResult ( m_contextName=" << res.m_contextName
+      << ", m_specName=" << res.m_specName
+      << ", m_success=" << res.m_success
+      << ", m_errorMessage=" << res.m_errorMessage
+      << " )";
+
+    return stream;
+  }
+
 }
 
 #endif /* IGLOO_TESTRESULT_H */
