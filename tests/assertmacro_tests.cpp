8c3008ba58fcfb49eeb5efa11c37a08f01526e84
Joakim Karlsson <joakim@jkarlsson.com>
2010-06-06 22:04:52 +0200

    Moved file and line numbers in to Assert::That method

--- tests/assertmacro_tests.cpp
+++ tests/assertmacro_tests.cpp
@@ -16,5 +16,29 @@ Context(AssertMacro)
     AssertThat(10, IsLessThan(20) && IsGreaterThan(5) && Equals(5+5));
   };
   
+  Spec(DisplaysFileAndLineNumberForErrors)
+  {
+    const char* file_name = "A file name";
+    int line_number = 453;
+
+    std::ostringstream expected_error_message;
+    
+    expected_error_message << file_name << "(" << line_number << "): Assertion failed" << std::endl;  
+
+    AssertTestFails(Assert::That(5, Equals(3), file_name, line_number), expected_error_message.str());
+  }
+
+  Spec(DisplaysFileAndLineNumberForFluentForm)
+  {
+    const char* file_name = "A file name";
+    int line_number = 453;
+
+    std::ostringstream expected_error_message;
+    
+    expected_error_message << file_name << "(" << line_number << "): Assertion failed" << std::endl;  
+
+    AssertTestFails(Assert::That(5, Is().EqualTo(3), file_name, line_number), expected_error_message.str());
+  }
+
 };
 
