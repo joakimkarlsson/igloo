5372bbc544524a693dd815064f454e52c91854df
Joakim Karlsson <joakim@jkarlsson.com>
2010-06-10 18:40:54 +0200

    Created include file for everything except aliases for registration macros

---
+++ igloo/igloo_framework.h
@@ -0,0 +1,33 @@
+
+//          Copyright Joakim Karlsson & Kim Gräsman 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef IGLOO_IGLOO_FRAMEWORK_H
+#define IGLOO_IGLOO_FRAMEWORK_H
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <stack>
+#include <list>
+#include <memory>
+
+#include <igloo/core/stringize.h>
+#include <igloo/constraints/constraints.h>
+#include <igloo/fluent/fluent.h>
+
+#include <igloo/core/assertionexception.h>
+#include <igloo/core/context.h> 
+#include <igloo/core/contextrunner.h>
+#include <igloo/core/testrunner.h>
+#include <igloo/core/assert.h>
+#include <igloo/core/assertmacro.h>
+#include <igloo/core/contextprovider.h>
+#include <igloo/core/registration.h>
+
+typedef igloo::ContextBase IGLOO_CURRENT_CONTEXT;
+
+#endif
