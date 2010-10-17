adddbbbef9f6c12497ad62db31a6183f2b836cfe
Joakim Karlsson <joakim@jkarlsson.com>
2010-10-17 20:14:20 +0200

    Moved code from exceptions_tests.cpp to exceptions.h

--- igloo/core/exceptions.h
+++ igloo/core/exceptions.h
@@ -10,32 +10,67 @@
 #include <igloo/core/assert.h>
 
 namespace igloo {
-  
-  template <typename ExceptionType, typename TypeUnderTest, typename MethodUnderTest>
-  ExceptionType Thrown(TypeUnderTest& objectUnderTest, MethodUnderTest method)
+   
+  template <typename ExceptionType>
+  class ExceptionStorage
   {
-    bool unknown_exception_caught = false;
-    
-    try 
+  public:
+    static std::auto_ptr<ExceptionType>& last_exception()
     {
-      (objectUnderTest.*method)();
-    }
-    catch (const ExceptionType& e) 
-    {
-      return e;
-    }
-    catch(...)
-    {
-      unknown_exception_caught = true;
+      static std::auto_ptr<ExceptionType> last;
+      return last;
     }
     
-    std::string error_msg = unknown_exception_caught ? "An exception of the wrong type was thrown" : "No exception thrown";
-    Assert::Failure(error_msg);
-    throw;
-  }
-  
+    void compiler_thinks_i_am_unused() {}
+    
+    ~ExceptionStorage()
+    {
+      last_exception().reset(NULL);
+    }
+  };
+    
+  template <typename ExceptionType>
+  inline ExceptionType& LastException()
+  {
+    if(ExceptionStorage<ExceptionType>::last_exception().get() == NULL)
+    {
+      Assert::Failure("No exception was stored");
+    }
+    
+    return *(ExceptionStorage<ExceptionType>::last_exception().get());
+  }  
 }
 
+#define CONCAT2(a, b) a##b
+#define CONCAT(a, b) CONCAT2(a, b)
+
+#define AssertThrows(EXCEPTION_TYPE, METHOD) \
+ExceptionStorage<EXCEPTION_TYPE> CONCAT(storage_, __LINE__); CONCAT(storage_, __LINE__).compiler_thinks_i_am_unused(); \
+{ \
+  bool wrong_exception = false; \
+  bool no_exception = false; \
+  try \
+  { \
+    METHOD; \
+    no_exception = true; \
+  } \
+  catch (const EXCEPTION_TYPE& e) \
+  { \
+    ExceptionStorage<EXCEPTION_TYPE>::last_exception() = std::auto_ptr<EXCEPTION_TYPE>(new EXCEPTION_TYPE(e)); \
+  } \
+  catch(...) \
+  { \
+    wrong_exception = true; \
+  } \
+  if(no_exception) \
+  { \
+    Assert::Failure("No exception was thrown"); \
+  } \
+  if(wrong_exception) \
+  { \
+    Assert::Failure("Wrong exception was thrown"); \
+  } \
+}
 
 #endif
 
--- tests/exceptions_tests.cpp
+++ tests/exceptions_tests.cpp
@@ -9,65 +9,6 @@
 
 using namespace igloo;
 
-#define CONCAT2(a, b) a##b
-#define CONCAT(a, b) CONCAT2(a, b)
-
-#define AssertThrows(EXCEPTION_TYPE, METHOD) \
-ExceptionStorage<EXCEPTION_TYPE> CONCAT(storage_, __LINE__); CONCAT(storage_, __LINE__).dummy(); \
-{ \
-  bool wrong_exception = false; \
-  bool no_exception = false; \
-  try \
-  { \
-    METHOD; \
-    no_exception = true; \
-  } \
-  catch (const EXCEPTION_TYPE& e) \
-  { \
-    ExceptionStorage<EXCEPTION_TYPE>::last_exception = std::auto_ptr<EXCEPTION_TYPE>(new EXCEPTION_TYPE(e)); \
-  } \
-  catch(...) \
-  { \
-    wrong_exception = true; \
-  } \
-  if(no_exception) \
-  { \
-    Assert::Failure("No exception was thrown"); \
-  } \
-  if(wrong_exception) \
-  { \
-    Assert::Failure("Wrong exception was thrown"); \
-  } \
-}
-
-template <typename ExceptionType>
-class ExceptionStorage
-{
-public:
-  static std::auto_ptr<ExceptionType> last_exception;
-  
-  void dummy() {}
-  
-  ~ExceptionStorage()
-  {
-    last_exception.reset(NULL);
-  }
-};
-
-template <typename ExceptionType>
-std::auto_ptr<ExceptionType> ExceptionStorage<ExceptionType>::last_exception(NULL);
-
-template <typename ExceptionType>
-inline ExceptionType& LastException()
-{
-  if(ExceptionStorage<ExceptionType>::last_exception.get() == NULL)
-  {
-    Assert::Failure("No exception was stored");
-  }
-  
-  return *(ExceptionStorage<ExceptionType>::last_exception.get());
-}
-
 class ClassWithExceptions
 {
 public:
