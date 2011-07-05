219cb0af6d53271a11815f9ecb91f4c34f009507
Joakim Karlsson <joakim@jkarlsson.com>
2011-07-05 16:52:33 +0200

    Added test case for output with failed test in result

---
+++ tests/defaulttestresultsoutput_tests.cpp
@@ -0,0 +1,53 @@
+
+//          Copyright Joakim Karlsson & Kim Gräsman 2010.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <tests/igloo_self_test.h>
+#include <memory>
+
+using namespace igloo;
+
+Context(DefaultTestResultsOutput_EmptyTestRun)
+{
+  std::auto_ptr<DefaultTestResultsOutput> output;
+  std::stringstream resulting_stream;
+  TestResults results;
+
+  void SetUp()
+  {
+    output = std::auto_ptr<DefaultTestResultsOutput>(new DefaultTestResultsOutput(resulting_stream));
+  }
+
+  Spec(OutputsASummaryLineWithNoTests)
+  {
+    output->PrintResult(results);
+
+    Assert::That(resulting_stream.str(), Is().StartingWith("Test run complete. 0 tests run, 0 succeeded, 0 failed."));
+  }
+
+  Context(DefaultTestResultsOutput_OneFailedTest)
+  {
+    void SetUp()
+    {
+      testResults().push_back(TestResult("A context name", "A spec name", false, "The error message"));
+    }
+
+    Spec(DisplaysOneFailedTestOnSummary)
+    {
+      Parent().output->PrintResult(Parent().results);
+      Assert::That(resulting_output(), Has().Exactly(1).StartingWith("Test run complete. 1 tests run, 0 succeeded, 1 failed."));
+    }
+
+    TestResults& testResults() 
+    {
+      return Parent().results;
+    }
+    
+    std::string resulting_output() 
+    {
+      return Parent().resulting_stream.str();
+    }
+  };
+};
