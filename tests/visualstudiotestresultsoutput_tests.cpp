3edc505189dd4c13563b18a2c46fa0e520747a5e
Joakim Karlsson <joakim@jkarlsson.com>
2011-07-26 18:03:01 +0200

    Added Visual Studio output without file and line information

--- tests/visualstudiotestresultsoutput_tests.cpp
+++ tests/visualstudiotestresultsoutput_tests.cpp
@@ -18,6 +18,13 @@ class VisualStudioResultsOutput
 
     void PrintResult(const TestResults& results)
     {
+      TestResults::FailedTestsType::const_iterator it;
+
+      for(it = results.FailedTests().begin(); it != results.FailedTests().end(); it++)
+      {
+        output_ << "Igloo : assertion failed error I001: " << (*it).GetContextName() << "::" << (*it).GetSpecName() << ": " << (*it).GetErrorMessage() << std::endl;
+      }
+
       output_ << "Test run complete. " << results.NumberOfTestsRun() << " tests run, " << results.NumberOfSucceededTests() << " succeeded, " << results.NumberOfFailedTests() << " failed." << std::endl;
     }
 
@@ -34,12 +41,40 @@ Context(VisualStudioResultsOutput_EmptyTestRun)
   void SetUp()
   {
     output = std::auto_ptr<VisualStudioResultsOutput>(new VisualStudioResultsOutput(resulting_stream));
+    output->PrintResult(results);
   }
 
   Spec(it_should_display_a_summary_line_with_no_tests_run)
   {
-    output->PrintResult(results);
-
     Assert::That(resulting_stream.str(), Is().StartingWith("Test run complete. 0 tests run, 0 succeeded, 0 failed."));
   }
+
+  Context(VisualStudioResultsOutput_one_failed_test_with_no_location_information)
+  {
+    void SetUp()
+    {
+      testResults().AddResult(FailedTestResult("A context name", "A failing spec", "The error message"));
+      Parent().output->PrintResult(Parent().results);
+    }
+
+    Spec(it_displays_one_failing_test_in_summary)
+    {
+      AssertThat(resulting_output(), Has().Exactly(1).StartingWith("Test run complete. 1 tests run, 0 succeeded, 1 failed"));
+    }
+
+    Spec(it_displays_error_text_for_failed_spec_with_no_location_information)
+    {
+      AssertThat(resulting_output(), Is().Containing("Igloo : assertion failed error I001: A context name::A failing spec: The error message"));
+    }
+
+    TestResults& testResults() 
+    {
+      return Parent().results;
+    }
+    
+    std::string resulting_output() 
+    {
+      return Parent().resulting_stream.str();
+    }
+  };
 };
