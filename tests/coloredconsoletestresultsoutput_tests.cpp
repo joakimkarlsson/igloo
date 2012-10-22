c5a5821be5618ecb0d3be98a6c14ea5d56b59314
Eric D. Reichert <eric@reichertconsulting.com>
2012-10-22 12:33:51 -0400

    Finished reformatting the test summary.

--- tests/coloredconsoletestresultsoutput_tests.cpp
+++ tests/coloredconsoletestresultsoutput_tests.cpp
@@ -65,32 +65,42 @@ Context(ColoredConsoleTestResultsOutput_EmptyTestRun)
     }
   };
 
-//  Context(ColoredConsoleTestResultsOutput_one_failed_test_with_information_about_origin_of_assertion)
-//  {
-//    void SetUp()
-//    {
-//      testResults().AddResult(FailedTestResult("A context name", "A spec name", "The error message", "filewithfailure.cpp", 342));
-//      Parent().output->PrintResult(Parent().results);
-//    }
-//
-//    Spec(DisplaysOneFailedTestOnSummary)
-//    {
-//      Assert::That(resulting_output(), Has().Exactly(1).StartingWith("Test run complete. 1 tests run:\n0 succeeded\n1 failed"));
-//    }
-//
-//    Spec(DisplaysErrorTextForFailedTestCorrectly)
-//    {
-//      Assert::That(resulting_output(), Is().StartingWith("A context name::A spec name failed:\nfilewithfailure.cpp(342): Assertion failed.\nThe error message"));
-//    }
-//
-//    TestResults& testResults()
-//    {
-//      return Parent().results;
-//    }
-//
-//    std::string resulting_output()
-//    {
-//      return Parent().resulting_stream.str();
-//    }
-//  };
+  Context(ColoredConsoleTestResultsOutput_one_failed_test_with_information_about_origin_of_assertion)
+  {
+    void SetUp()
+    {
+      testResults().AddResult(FailedTestResult("A context name", "A spec name", "The error message", "filewithfailure.cpp", 342));
+      Parent().output->PrintResult(Parent().results);
+    }
+
+    Spec(DisplaysOneTestsRunOnSummary)
+	{
+		Assert::That(resulting_output(), Has().Exactly(1).StartingWith("Test run complete. 1 tests run:"));
+	}
+
+	Spec(DisplaysZeroSuccessfulTestsOnSummary)
+	{
+		Assert::That(resulting_output(), Has().Exactly(1).StartingWith("0 succeeded"));
+	}
+
+	Spec(DisplaysOneFailedTestOnSummary)
+	{
+		Assert::That(resulting_output(), Has().Exactly(1).StartingWith("1 failed"));
+	}
+
+    Spec(DisplaysErrorTextForFailedTestCorrectly)
+    {
+      Assert::That(resulting_output(), Is().StartingWith("A context name::A spec name failed:\nfilewithfailure.cpp(342): Assertion failed.\nThe error message"));
+    }
+
+    TestResults& testResults()
+    {
+      return Parent().results;
+    }
+
+    std::string resulting_output()
+    {
+      return Parent().resulting_stream.str();
+    }
+  };
 };
