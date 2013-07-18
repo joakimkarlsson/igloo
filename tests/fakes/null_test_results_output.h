401a6ea64b260a4cdbc0c75805f2c3d28ae8f618
Joakim Karlsson <joakim@jkarlsson.com>
2013-07-18 10:54:28 +0200

    chore: Updated year in copyright notice.

--- tests/fakes/null_test_results_output.h
+++ tests/fakes/null_test_results_output.h
@@ -1,7 +1,7 @@
 #ifndef _IGLOO_TESTS_FAKES_NULL_TEST_RESULTS_OUTPUT_H
 #define _IGLOO_TESTS_FAKES_NULL_TEST_RESULTS_OUTPUT_H
 
-//          Copyright Joakim Karlsson & Kim Gräsman 2010-2012.
+//          Copyright Joakim Karlsson & Kim Gräsman 2010-2013.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
