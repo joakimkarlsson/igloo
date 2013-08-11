da1ddfd72044a4fe559aaa87af97697bfa8a3cc7
Joakim Karlsson <joakim@jkarlsson.com>
2013-08-11 10:36:33 +0200

    fix(snowhouse): restored snowhouse's includes
    This is needed so that snowhouse can be used standalone.

--- igloo/external/snowhouse/snowhouse/snowhouse.h
+++ igloo/external/snowhouse/snowhouse/snowhouse.h
@@ -3,6 +3,15 @@
 
 #define SNOWHOUSE_VERSION "1.0.1"
 
+#include <iostream>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <stack>
+#include <list>
+#include <memory>
+#include <algorithm>
+
 #include "stringize.h"
 #include "constraints/constraints.h"
 #include "fluent/fluent.h"
@@ -12,4 +21,3 @@
 #include "exceptions.h"
 
 #endif
-
