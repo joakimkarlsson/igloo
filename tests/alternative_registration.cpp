033e714a72be60732ffdf1921cf34900106a22fe
Joakim Karlsson <joakim@jkarlsson.com>
2013-08-17 06:40:44 -0700

    chore(build): precompiled headers via cotire

--- tests/alternative_registration.cpp
+++ tests/alternative_registration.cpp
@@ -4,6 +4,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <tests/igloo_self_test.h>
 #include <igloo/igloo_alt.h>
 using namespace igloo;
 
