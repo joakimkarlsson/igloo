86188c1f283483305686de2ac6b9989d2ddcd960
Joakim Karlsson <joakim@jkarlsson.com>
2013-12-23 17:09:24 +0100

    chore: removed cmake artifacts

--- cotire/snowhouse-tests_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.4.1 generated file */
-/* /Users/jok/Documents/sw/github/igloo/cotire/snowhouse-tests_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/Users/jok/Documents/sw/github/igloo/tests/igloo_self_test.h"
-#endif
