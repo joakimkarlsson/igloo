23af4a905c72ee97b2a0a21813054d3ce7120f2b
Joakim Karlsson <joakim@jkarlsson.com>
2015-07-21 11:18:24 +0200

    Adds test to verify namespaces in macros fix.
    Adds tests to verify that we can use igloo without
    'using namespace igloo;'. Addresses PR #18.

---
+++ tests/explicit_namespace_import.cpp
@@ -0,0 +1,23 @@
+
+//          Copyright Joakim Karlsson & Kim Gräsman 2010-2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <igloo/igloo_alt.h>
+
+//
+// Ensure that we can use igloo without explicitly importing all of the 'igloo'
+// namespace.
+//
+
+using snowhouse::Assert;
+using snowhouse::Equals;
+
+Describe(can_be_used_without_importing_whole_namespace)
+{
+    It(does_a_silly_assert)
+    {
+        Assert::That(5, Equals(5));
+    }
+};
